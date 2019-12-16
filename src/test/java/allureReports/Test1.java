7c8f97b83bf73f6777d41382fde0a01417b43be1
LisaJomy <lisa.selenium@gmail.com>
2019-12-16 09:26:54 -0500

    first commit

---
+++ src/test/java/allureReports/Test1.java
@@ -0,0 +1,70 @@
+package allureReports;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	WebDriver driver;
+	@BeforeClass
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	
+	driver.get("https://demo.nopcommerce.com/");
+	driver.manage().window().maximize();
+
+	
+	}	
+	
+ @Test(priority=1 )	
+public void logoPresence()	{
+boolean disstatus=driver.findElement(By.xpath("//div[@class='header-logo']//a//img")).isDisplayed();	 
+	Assert.assertEquals(disstatus,true);
+}
+	
+@Test(priority=2)
+public void loginTest() {
+	
+	driver.findElement(By.linkText("Log in")).click();
+	driver.findElement(By.id("Email")).sendKeys("lisa_k_y@yahoo.com");
+	driver.findElement(By.id("Password")).sendKeys("Pudukad@123");
+	driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
+	
+	Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Login");
+	
+}
+	
+@Test(priority=3)	
+	
+public void regestrationTest()	{
+	throw new SkipException("skipping this test");
+	
+}
+	
+@AfterClass
+public void tearDown() {
+	driver.quit();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
